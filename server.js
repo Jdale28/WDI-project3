52dcad1bf0fd6913d768a1e4be0e95927cfc6f17
Jordan Dale <jordan.dale2@gmail.com>
2018-11-12 17:22:32 -0500

    Concurrently running

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
-require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
 const app = express();
-mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/my-project3
+// const routes = require('./routes/index')
 
-const connection = mongoose.connection;
-connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully')
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// If the connection throws an error
-connection.on('error', (err) => {
-  console.log('Mongoose default connection error: ' + err);
-}) 
+app.use(express.static(__dirname + "/client/build/"));
 
-app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+// app.use('/', routes)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
-})
+  console.log(`Server is listening on PORT: ${PORT}`);
+});

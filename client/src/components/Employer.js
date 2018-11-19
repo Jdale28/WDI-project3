b4b9b5328db0e483dc4e8cb6bdb5428a24e0e6a1
Jordan Dale <jordan.dale2@gmail.com>
2018-11-19 10:52:57 -0500

    committing for reverting if needed

--- client/src/components/Employer.js
+++ client/src/components/Employer.js
@@ -3,19 +3,28 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const NewEmployeeButton = styled.button`
-  background: #1d3557;
+const EmployeeContainer = styled.div`
+  border: 1px solid black;
+  width: 50%;
+  text-decoration: none;
   color: white;
   font-size: 1.3rem;
   padding: 7.5px 5px;
+  a {
+    text-decoration: none;
+  }
+  h2 {
+    color: red;
+    text-decoration: none;
+  }
 `;
 
 class Employer extends Component {
   state = {
     newEmployee: {
-      fullName: '',
-      email: '',
-      jobTitle: '',
+      fullName: "",
+      email: "",
+      jobTitle: ""
     },
     employer: {},
     employees: [{}]
@@ -34,26 +43,26 @@ class Employer extends Component {
     });
   };
 
-  handleChange = (event) => {
-    const updatedNewEmployee = {...this.state.newEmployee}
-    updatedNewEmployee[event.target.name] = event.target.value
-    this.setState({newEmployee: updatedNewEmployee})
-  }
-  
-  handleSubmit = (event) => {
-    const employerId = this.props.match.params.employerId
-    event.preventDefault()
+  handleChange = event => {
+    const updatedNewEmployee = { ...this.state.newEmployee };
+    updatedNewEmployee[event.target.name] = event.target.value;
+    this.setState({ newEmployee: updatedNewEmployee });
+  };
+
+  handleSubmit = event => {
+    const employerId = this.props.match.params.employerId;
+    event.preventDefault();
     const payload = {
       fullName: this.state.newEmployee.fullName,
-    }
+      jobTitle: this.state.newEmployee.jobTitle,
+      email: this.state.newEmployee.email
+    };
     axios.post(`/api/employers/${employerId}/employees`, payload).then(res => {
-      const newEmployee = res.data
-      const newStateNewEmployee = [...this.state.employees, newEmployee]
-      this.setState({ employees: newStateNewEmployee })
-    })
-  }
-
-
+      const newEmployee = res.data;
+      const newStateNewEmployee = [...this.state.employees, newEmployee];
+      this.setState({ employees: newStateNewEmployee });
+    });
+  };
 
   render() {
     return (
@@ -62,28 +71,58 @@ class Employer extends Component {
         <h4>Your employees include:</h4>
         {this.state.employees.map(employee => (
           <div key={employee._id}>
-            <Link
-              to={`/api/employers/${this.props.match.params.employerId}/employees/${employee._id}`}>
-              {employee.fullName}
-            </Link>
-            <Link
+            <EmployeeContainer>
+              <Link
+                to={`/api/employers/${
+                  this.props.match.params.employerId
+                }/employees/${employee._id}`}
+              >
+                <h2>Name: {employee.fullName}</h2>
+                <h4>Job Title: {employee.jobTitle}</h4>
+                <h4>Email: {employee.email}</h4>
+              </Link>
+            </EmployeeContainer>
+            {/* <Link
               to={`/api/employers/${this.props.match.params.employerId}/employees/${employee._id}`}>
               Edit Employee
-            </Link>
+            </Link> */}
             {/* <button>>Edit Employee</button> */}
           </div>
         ))}
 
+        <div>
+          <form onSubmit={this.handleSubmit}>
             <div>
-            <form onSubmit={this.handleSubmit}>
-              <div>
-                <label htmlFor="fullName">Full Name: </label>
-                <input onChange={this.handleChange} value={this.state.newEmployee.fullName} type="text" name="fullName"/>
-              </div>
+              <label htmlFor="fullName">Full Name: </label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.newEmployee.fullName}
+                type="text"
+                name="fullName"
+              />
+            </div>
+            <div>
+              <label htmlFor="jobTitle">Job Title: </label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.newEmployee.jobTitle}
+                type="text"
+                name="jobTitle"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email: </label>
+              <input
+                onChange={this.handleChange}
+                value={this.state.newEmployee.email}
+                type="text"
+                name="email"
+              />
+            </div>
 
-              <button type="submit">Create New Employee</button>
-            </form>
-          </div>
+            <button type="submit">Create New Employee</button>
+          </form>
+        </div>
       </div>
     );
   }
